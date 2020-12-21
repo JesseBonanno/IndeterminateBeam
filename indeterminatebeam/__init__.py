35793843a938cdf3da9a8182edbc4f7251cf2ed5
JesseBonanno <JesseBonanno@gmail.com>
2020-12-21 11:40:43 +1100

    v1.0.2 - data validation update

--- indeterminatebeam/__init__.py
+++ indeterminatebeam/__init__.py
@@ -1,2 +1,2 @@
-from indeterminatebeam.indeterminatebeam import Support, Beam, PointLoad, PointLoadV, PointLoadH, DistributedLoadH, DistributedLoadV, PointTorque, TrapezoidalLoad
 import indeterminatebeam.data_validation
+from indeterminatebeam.indeterminatebeam import Support, Beam, PointLoad, PointLoadV, PointLoadH, DistributedLoadH, DistributedLoadV, PointTorque, TrapezoidalLoad
