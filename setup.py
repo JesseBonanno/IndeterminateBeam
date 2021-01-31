9ab8e63f14dfd1cfa68152bfa6fc6f2e00663971
JesseBonanno <JesseBonanno@gmail.com>
2021-01-31 20:24:10 +1100

    version update

--- setup.py
+++ setup.py
@@ -24,10 +24,7 @@ setup(
           'matplotlib',
           'numpy',
           'sympy>=1.4',
-          'sphinxcontrib.bibtex',
-          'sphinx',
           'plotly>=4.14.1',
-          'kaleido',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
