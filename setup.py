13aa448cb9e565f59f85a684ea35d1c0e07cea4b
Dara Adib <daradib@ocf.berkeley.edu>
2021-12-26 17:22:17 -0500

    Allow latest SymPy

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
   install_requires=[            
           'matplotlib',
           'numpy',
-          'sympy>=1.4,<1.8',
+          'sympy>=1.4',
           'plotly>=4.14.1',
       ],
   classifiers=[
